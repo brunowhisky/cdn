73159883b77c2cb370a043c2da347d921da9fd75
brunowhisky <32206689+brunowhisky@users.noreply.github.com>
2017-09-22 16:57:16 -0300

    Update automacao_residencial.js

--- automacao_residencial.js
+++ automacao_residencial.js
@@ -7,7 +7,7 @@ function AlteraRele1(){
     } else {
         document.getElementById("estado").innerHTML="0";
         document.getElementById("rele").innerHTML="<div class='rele1_desligado'></div>";
-        document.getElementById("botao").innerHTML="<a href='/?ligar' class='botao'>Liga</a>";
+        document.getElementById("botao").innerHTML="<a href='/?ligar' class='botao'>Liga teste</a>";
     }
 }
 
